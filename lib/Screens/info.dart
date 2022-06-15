e45c12d0cdc01eeadb6928d724bc08cc2e76724d
nogreenJ <joaoldp135@gmail.com>
2022-06-15 14:45:11 -0300

    Primeira versão do texto de apresentação

--- lib/Screens/info.dart
+++ lib/Screens/info.dart
@@ -6,6 +6,23 @@ import 'package:projcyberbullying/Widgets/widget.dart';
 import 'package:provider/provider.dart';
 
 class Informacao extends StatelessWidget {
+  String texto1 = "Spearmint é uma ferramenta para armazenamento de provas de " +
+      "cyberbullying por meio de tecnologias distribuídas, garantindo, " +
+      "assim, a impermutabilidade desses dados e seu fácil armazenamento de forma " +
+      "transparente, soluciondo a burocracia que seria o seu registro.";
+
+  String texto2 =
+      "Para realizar as capturas, foi criada uma extensão complementar à aplicação, " +
+          "que pode ser obtida AQUI. Ao utilizá-la, é possível gerar um PDF " +
+          "contendo uma captura de tela de qualquer página da web, assim como um conjunto de dados " +
+          "sobre a captura, coletados em via a validá-la.";
+
+  String texto3 = "As provas são armazenadas em um sistema chamado IPFS, Interplanetary File System, " +
+      "que, armazenando dados de forma distribuída, garante que sejam imutáveis e " +
+      "sempre acessíveis, enquanto a indexação de tais dados com os usuários " +
+      "é feita por meio da tecnologia Blockchain, tornando esses registros permanentes" +
+      "em um sistema confiável.";
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -20,12 +37,27 @@ class Informacao extends StatelessWidget {
             child: Column(
               children: [
                 RichText(
-                  text: TextSpan(
-                    text: 'test',
-                    style: TextStyle(color: Colors.green, fontSize: 25),
-                  ),
-                )
+                    text: TextSpan(children: <TextSpan>[
+                  generateText("Sobre o Spearmint", texto1),
+                  generateText("Como Utilizá-lo", texto2),
+                  generateText("Como Funciona", texto3),
+                ]))
               ],
             )));
+  }
+
+  TextSpan generateText(String title, String texto) {
+    return TextSpan(
+        text: title,
+        style: TextStyle(
+            color: Colors.green, fontSize: 25, fontWeight: FontWeight.bold),
+        children: <TextSpan>[
+          TextSpan(
+              text: "\n" + texto + "\n\n",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.normal))
+        ]);
   }
 }
